f5008a906a2755ba87b522c0eb5336e833fc76b6
Cara Morelli <caramorelli7@gmail.com>
2018-08-19 23:03:13 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -4,8 +4,8 @@ function addElementToBeginningOfArray(array, element) {
   var new_array = []
   new_array.push(element)
   for (var i = 0; i < array.length; i++) {
-    var ele = array[i]
-    new_array.push()
+    var ele = array[i];
+    new_array.push(ele)
   }
   return new_array;
 }
