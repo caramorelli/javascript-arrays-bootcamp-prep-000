a4f5cbd1f86444c37ce8da41990d7817a6ac306c
Cara Morelli <caramorelli7@gmail.com>
2018-08-19 23:00:45 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,8 +1,11 @@
 // var chocolateBars = [`snickers`, `hundred grand`, `kitkat`, `skittles`]
 
 function addElementToBeginningOfArray(array, element) {
-  var new_array = [];
-  for (var i = 0, i < array.length, i++) 
+  var new_array = []
+  for (var i = 0, i < array.length, i++) {
+    
+  }
+  return new_array;
 }
 
 function destructivelyAddElementToBeginningOfArray(array, elemnt) {
